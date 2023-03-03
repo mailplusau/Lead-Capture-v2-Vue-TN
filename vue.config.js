d097c641a9ce9279d0cb4c88f38058fe93e76b98
Thien Nguyen <s3426515@rmit.edu.vn>
2023-03-03 11:18:46 +1100

    Single HTML file compiling works

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  css: {
+    extract: false,
+  },
+  configureWebpack: {
+    optimization: {
+      splitChunks: false // makes there only be 1 js file - leftover from earlier attempts but doesn't hurt
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'mp_cl_lead_capture_tn_v2.html',
+        template: 'public/index.html',  //template file to embed the source
+        inlineSource: '.(js|css)$' // embed all javascript and css inline
+      }),
+      new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin)
+    ]
+  }
 }
