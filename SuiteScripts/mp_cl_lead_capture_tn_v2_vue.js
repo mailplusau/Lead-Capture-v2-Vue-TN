0bef0b88bbf258e16392f6e460d90e2143148bed
Tim Nguyen <tim.nguyen@mailplus.com.au>
2024-02-13 09:08:44 +1100

    Alternative way to load vue app

--- SuiteScripts/mp_cl_lead_capture_tn_v2_vue.js
+++ SuiteScripts/mp_cl_lead_capture_tn_v2_vue.js
@@ -19,6 +19,38 @@ define(moduleNames.map(item => 'N/' + item), (...args) => {
             console.log('Client script init.');
             document.querySelector('h1.uir-record-type').innerHTML = 'Loading page. Please wait...';
 
+            try {
+                requestAppViaXMLHttpRequest();
+            } catch (e) {
+                console.error('requestAppViaXMLHttpRequest failed:', e);
+                requestAppViaFetch();
+            }
+        }
+        
+        function requestAppViaXMLHttpRequest() {
+            if (!document.querySelector('div#body')) return;
+            console.log('attempting requestAppViaXMLHttpRequest...');
+
+            let request = new XMLHttpRequest();
+
+            request.onreadystatechange = function() {
+                if (request.readyState === XMLHttpRequest.DONE) { // XMLHttpRequest.DONE === 4
+                    if (request.status === 200) injectVueApp(JSON.parse(request.responseText))
+                    else {
+                        console.error('requestAppViaXMLHttpRequest failed with status', request.status)
+                        requestAppViaFetch();
+                    }
+                }
+            };
+
+            request.open("GET", "scriptlet.nl?script=1706&deploy=1&requestData=%7B%22operation%22%3A%22getIframeContents%22%7D", true);
+            request.send();
+        }
+
+        function requestAppViaFetch() {
+            if (!document.querySelector('div#body')) return;
+            console.log('attempting requestAppViaFetch...');
+
             let baseUrl = getCurrentNetSuiteUrl().split('?')[0];
             const params = new Proxy(new URLSearchParams(window.location.search), {
                 get: (searchParams, prop) => searchParams.get(prop),
@@ -33,18 +65,18 @@ define(moduleNames.map(item => 'N/' + item), (...args) => {
                 method: 'get'
             })
                 .then(res => res.json())
-                .then(jsonData => {
-                    let iframe = document.createElement('iframe');
-                    iframe.id = 'body';
-                    iframe.srcdoc = jsonData;
-                    iframe.style.setProperty('border', 'none');
-                    document.querySelector('div#body').parentNode.insertBefore(iframe, null);
-                    document.querySelector('div#body').remove();
-                    setMPTheme();
-                })
-                .catch(err => {
-                    console.log(err);
-                });
+                .then(jsonData => { injectVueApp(jsonData); })
+                .catch(err => { console.error('requestAppViaFetch failed:', err); });
+        }
+        
+        function injectVueApp(jsonData) {
+            let iframe = document.createElement('iframe');
+            iframe.id = 'body';
+            iframe.srcdoc = jsonData;
+            iframe.style.setProperty('border', 'none');
+            document.querySelector('div#body').parentNode.insertBefore(iframe, null);
+            document.querySelector('div#body').remove();
+            setMPTheme();
         }
 
         return { pageInit };
